73c88b7a1a5db169d7138ffbea00ac5a7a78052d
Casey Miller <caseymm@gmail.com>
2020-07-04 13:48:00 -0700

    session storage

---
+++ whos-wearing-a-mask/scripts/submit.js
@@ -0,0 +1,59 @@
+import './intersection-observer.chunk.js';
+
+// packages
+console.log('submit page');
+
+if (sessionStorage.getItem("logger")) {
+  document.getElementById('logger').value = sessionStorage.getItem("logger");
+}
+
+if (sessionStorage.getItem("neighborhood")) {
+  document.getElementById('neighborhood').value = sessionStorage.getItem("neighborhood");
+}
+
+const form = document.getElementById('form-submit');
+
+const formToJSON = elements => [].reduce.call(elements, (data, element) => {
+  if (element.type === 'radio') {
+    if (element.checked) {
+      data[element.name] = element.value;
+    }
+  } else {
+    data[element.name] = element.value;
+  }
+
+  return data;
+}, {});
+
+const submitForm = e => {
+  e.preventDefault();
+  e.stopPropagation();
+  e.stopImmediatePropagation();
+  let formJSON = formToJSON(form.elements);
+  const date = new Date();
+
+  const getMonth = month => {
+    if (month < 10) {
+      return `0${month}`;
+    } else {
+      return month;
+    }
+  };
+
+  const getFullDate = `${date.getFullYear()}-${getMonth(date.getMonth() + 1)}-${date.getDate()}`;
+  formJSON.date = getFullDate;
+  formJSON.time = date;
+
+  if (formJSON.logger.length > 0 && formJSON.logger !== sessionStorage.getItem("logger")) {
+    sessionStorage.setItem('logger', formJSON.logger);
+  }
+
+  if (formJSON.neighborhood.length > 0 && formJSON.neighborhood !== sessionStorage.getItem("neighborhood")) {
+    sessionStorage.setItem('neighborhood', formJSON.neighborhood);
+  }
+
+  console.log(formJSON); // post(formJSON);
+};
+
+form.addEventListener('submit', submitForm);
+//# sourceMappingURL=submit.js.map
